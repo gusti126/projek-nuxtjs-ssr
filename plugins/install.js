87632fd544bcc919491fa2549a00c267870d7c58
gusti126 <gustidoang936@gmail.com>
2021-08-30 00:50:54 +0700

    change galang dana

--- plugins/install.js
+++ plugins/install.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueFlicking from '@egjs/vue-flicking'
 import Flicking from '@egjs/vue-flicking'
 import '@egjs/vue-flicking/dist/flicking.css'
+import '@egjs/vue-flicking/dist/flicking-inline.css'
 
 Vue.use(VueFlicking)
 Vue.use(Flicking)
