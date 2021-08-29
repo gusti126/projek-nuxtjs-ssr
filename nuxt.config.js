92f54e4cbe2fdd46ff124110252d69ab9e2e11e4
gusti126 <gustidoang936@gmail.com>
2021-08-30 00:17:50 +0700

    change nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -110,5 +110,14 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    loaders: {
+      vue: {
+        compilerOptions: {
+          // This option is highly recommended
+          preserveWhitespace: false,
+        },
+      },
+    },
+  },
 }
